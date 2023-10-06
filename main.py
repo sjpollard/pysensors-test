dbf8f1a3bbd139e619c828c0c723a780dec8d8d0
sjpollard <samjosephpollard@gmail.com>
2023-10-06 15:50:00 +0100

    EPIC toy problem working

--- main.py
+++ main.py
@@ -8,28 +8,25 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from pysensors.classification import SSPOC
-from PIL import Image
-
-
+from PIL import Image, ImageOps
 
 
 def main():
     # Set seed for reproducibility
-    random_state = 0
+    random_state = 1
 
-    display = False
+    display = True
 
     # Load the data
-    faces = datasets.fetch_olivetti_faces()
-    X = faces['data']
-    y = faces['target']
+    X, y, images = load_EPIC('../../EPIC-KITCHENS')
 
-    height, width = faces['images'][0].shape
+    height, width = images[0].shape
 
+    print(X.shape, y.shape)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=random_state)
 
-    n_basis_modes = 100
-    l1_penalty = 0.2
+    n_basis_modes = 20
+    l1_penalty = 0.000005
 
     #basis = ps.basis.Identity(n_basis_modes=n_basis_modes)
     basis = ps.basis.SVD(n_basis_modes=n_basis_modes)
@@ -41,14 +38,14 @@ def main():
     
     if display:
         
-        ts.show(faces['images'][0])
+        ts.show(images[0])
 
         plot_sensor_locations(model.selected_sensors, (height, width))
 
-        new_face = np.zeros(height * width)
-        np.put(new_face, model.get_selected_sensors(), faces['data'][0][model.get_selected_sensors()])
-        new_face = np.reshape(new_face, (height, width))
-        ts.show(new_face)
+        new_image = np.zeros(height * width)
+        np.put(new_image, model.get_selected_sensors(), X[0][model.get_selected_sensors()])
+        new_image = np.reshape(new_image, (height, width))
+        ts.show(new_image)
 
         ts.show(model.basis.matrix_representation())
 
@@ -64,8 +61,21 @@ def main():
 def load_EPIC(path):
     annotations = pd.read_csv('EPIC.csv')
     annotations = annotations[annotations['video_id'] == 'P01_01']
-    print(annotations['noun_class'].value_counts())
-    #ts.show(Image.open(f'{path}/P01/rgb_frames/P01_01/frame_0000000001.jpg'))
+    top_3 = annotations['noun_class'].value_counts()[:3].index.tolist()
+    annotations = annotations[annotations['noun_class'].isin(top_3)].reset_index(drop=True)
+    frames = list(map(str, annotations['stop_frame'].values))
+    X = []
+    images = []
+    for frame in frames:
+        zero = '0'
+        file_name = f'{(10 - len(frame)) * zero}{frame}'
+        x = Image.open(f'{path}/P01/rgb_frames/P01_01/frame_{file_name}.jpg').convert(mode='L')
+        X.append(np.ravel(np.asarray(x)/255.0))
+        images.append(np.asarray(x)/255.0)
+    X = np.stack(X, axis=0)
+    images = np.stack(images, axis=0)
+    y = np.array(annotations['noun_class'].tolist())
+    return X, y, images
 
 # Plot sensor locations
 def plot_sensor_locations(sensors, shape, ax=None):
@@ -84,4 +94,4 @@ def plot_sensor_locations(sensors, shape, ax=None):
     plt.show()
 
 if __name__ == "__main__":
-    load_EPIC('../../EPIC-KITCHENS')
+    main()
